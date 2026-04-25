a3d19bf2faad71e65e6b39b2a86e8006b175b854
smadhur <smadhur@reocelot.com>
2026-04-25 16:44:19 -0700

    Added textured pixel shader file
    Change-Id: I49de5fd6c4480e64253ce9203cfc781d541a7d4f

---
+++ pequod/shaders/textured.ps.hlsl
@@ -0,0 +1,21 @@
+Texture2D<float4> diffuse_texture : register(t0);
+SamplerState texture_sampler : register(s0);
+
+struct PSInput
+{
+    float4 position : SV_Position;
+    float4 color : COLOR0;
+    float2 uv : TEXCOORD0;
+};
+
+struct PSOutput
+{
+    float4 color : SV_Target0;
+};
+
+PSOutput Main(PSInput input)
+{
+    PSOutput output = (PSOutput) 0;
+    output.color = diffuse_texture.Sample(texture_sampler, input.uv);
+    return output;
+}
